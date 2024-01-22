822b5efe8a269898f130faa041927550312dae53
DariaaaP <barinova_f@list.ru>
2024-01-22 19:10:54 +0400

    added functionality for RandomChar componentand MarvelService.js

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,28 +1,42 @@
 class MarvelService {
-  _apiBase = "https://gateway.marvel.com:443/v1/public/";
-  _apiKey = "apikey=0481e59bf467b69f70671a30361d50b4";
+    _apiBase = "https://gateway.marvel.com:443/v1/public/";
+    _apiKey = "apikey=0481e59bf467b69f70671a30361d50b4";
 
-  getResource = async (url) => {
-    let res = await fetch(url);
+    getResource = async (url) => {
+        let res = await fetch(url);
 
-    if (!res.ok) {
-      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-    }
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
 
-    return await res.json();
-  };
+        return await res.json();
+    };
 
-  getAllCharacters = () => {
-    return this.getResource(
-      `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
-    );
-  };
+    getAllCharacters = async () => {
+        const res = await this.getResource(
+            `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
+        );
+        return res.data.results.map(this._transformCharacter);
+    };
 
-  getCharacter = (id) => {
-    return this.getResource(
-      `${this._apiBase}characters/${id}?&${this._apiKey}`
-    );
-  };
+    getCharacter = async (id) => {
+        const res = await this.getResource(
+            `${this._apiBase}characters/${id}?&${this._apiKey}`
+        );
+        return this._transformCharacter(res.data.results[0]);
+    };
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description
+                ? `${char.description.slice(0, 210)}...`
+                : `There is no description for this character`,
+            thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+        };
+    };
 }
 
 export default MarvelService;
