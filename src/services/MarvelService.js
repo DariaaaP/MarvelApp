e1e656a38e8f5f6ff9d2f99f7135e2b9554b63e4
DariaaaP <barinova_f@list.ru>
2024-01-22 18:15:24 +0400

    add new project w/layouts, styles and added services

---
+++ src/services/MarvelService.js
@@ -0,0 +1,28 @@
+class MarvelService {
+  _apiBase = "https://gateway.marvel.com:443/v1/public/";
+  _apiKey = "apikey=0481e59bf467b69f70671a30361d50b4";
+
+  getResource = async (url) => {
+    let res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+  };
+
+  getAllCharacters = () => {
+    return this.getResource(
+      `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
+    );
+  };
+
+  getCharacter = (id) => {
+    return this.getResource(
+      `${this._apiBase}characters/${id}?&${this._apiKey}`
+    );
+  };
+}
+
+export default MarvelService;
