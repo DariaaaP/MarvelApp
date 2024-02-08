72aea79ac62e85b1a82b4032c15088e0c4366f81
DariaaaP <barinova_f@list.ru>
2024-02-08 17:37:08 +0400

    added routes

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -6,12 +6,20 @@ const useMarvelService = () => {
     const _apiBase = "https://gateway.marvel.com:443/v1/public/";
     const _apiKey = "apikey=0481e59bf467b69f70671a30361d50b4";
     const _baseOffsetChar = 210;
+    const _baseOffsetComics = 359;
 
     const getAllCharacters = async (offset = _baseOffsetChar) => {
         const res = await request(
             `${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`
         );
         return res.data.results.map(_transformCharacter);
+    };
+
+    const getAllComics = async (offset = _baseOffsetComics) => {
+        const res = await request(
+            `${_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${_apiKey}`
+        );
+        return res.data.results.map(_transformComics);
     };
 
     const getCharacter = async (id) => {
@@ -33,7 +41,30 @@ const useMarvelService = () => {
         };
     };
 
-    return { loading, error, getAllCharacters, getCharacter, clearError };
+    const _transformComics = (comics) => {
+        return {
+            id: comics.id,
+            title: comics.title,
+            description: comics.description || "There is no description",
+            pageCount: comics.pageCount
+                ? `${comics.pageCount} p.`
+                : "No information about the number of pages",
+            thumbnail: comics.thumbnail.path + "." + comics.thumbnail.extension,
+            language: comics.textObjects[0]?.language || "en-us",
+            price: comics.prices[0].price
+                ? `${comics.prices[0].price}$`
+                : "not available",
+        };
+    };
+
+    return {
+        loading,
+        error,
+        getAllCharacters,
+        getCharacter,
+        getAllComics,
+        clearError,
+    };
 };
 
 export default useMarvelService;
